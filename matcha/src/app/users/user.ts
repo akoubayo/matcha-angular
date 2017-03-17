32bce669bc8493d556cdeb8ae4a6245799323881
Damien <damien@tilkee.com>
2017-03-17 09:55:09 +0100

    clean for new version

--- matcha/src/app/users/user.ts
+++
@@ -1,9 +0,0 @@
-export class User {
-  public constructor(usr = null){
-        if (usr) {
-            for (let key in usr) {
-                    this[key] = usr[key];
-            }
-        }
-    }
-}
